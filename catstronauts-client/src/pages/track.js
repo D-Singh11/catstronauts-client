f24de28d30fa40dc213c30abc1c9fa34927814af
setname <setemail>
2022-05-27 16:35:35 +0100

    Call query and pass trackId to variables parameter

--- catstronauts-client/src/pages/track.js
+++ catstronauts-client/src/pages/track.js
@@ -1,32 +1,35 @@
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 import React from 'react'
 import { Layout } from '../components'
 
 const GET_TRACK = gql`
-query getTrack($trackId: ID!){
-    track(id: $trackId) {
-        id
-        modulesCount
-        modules {
+    query getTrack($trackId: ID!){
+        track(id: $trackId) {
             id
+            modulesCount
+            modules {
+                id
+                title
+                length
+            }
             title
+            author {
+                id
+                name
+                photo
+            }
+            thumbnail
             length
+            description
+            numberOfViews
         }
-        title
-        author {
-            id
-            name
-            photo
-        }
-        thumbnail
-        length
-        description
-        numberOfViews
     }
-}
-`
-;
-export const Track = () => {
+`;
+export const Track = ({trackId}) => {
+    const {loading, error, data } = useQuery(GET_TRACK, {
+        variables: {trackId}
+    });
+    console.log(JSON.stringify(data));
   return (
     <Layout>layout</Layout>
   )
