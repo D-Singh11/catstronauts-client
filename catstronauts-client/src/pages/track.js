c64be72339d2015fa3d4f9688eba66c72c8566e7
setname <setemail>
2022-05-27 16:15:32 +0100

    Build new query to get track's data

--- catstronauts-client/src/pages/track.js
+++ catstronauts-client/src/pages/track.js
@@ -1,6 +1,31 @@
+import { gql } from '@apollo/client';
 import React from 'react'
 import { Layout } from '../components'
 
+const GET_TRACK = gql`
+query getTrack($trackId: ID!){
+    track(id: $trackId) {
+        id
+        modulesCount
+        modules {
+            id
+            title
+            length
+        }
+        title
+        author {
+            id
+            name
+            photo
+        }
+        thumbnail
+        length
+        description
+        numberOfViews
+    }
+}
+`
+;
 export const Track = () => {
   return (
     <Layout>layout</Layout>
