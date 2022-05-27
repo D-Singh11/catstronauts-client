fd936536e08ae5e2e2838681657f3c8304ab5e19
setname <setemail>
2022-05-27 16:47:12 +0100

    Use TrackDetail component to display returned data

--- catstronauts-client/src/pages/track.js
+++ catstronauts-client/src/pages/track.js
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react'
 import { Layout } from '../components'
+import TrackDetail from '../components/track-detail';
 
 const GET_TRACK = gql`
     query getTrack($trackId: ID!){
@@ -25,12 +26,18 @@ const GET_TRACK = gql`
         }
     }
 `;
+
+/**
+ * Track Page fetches a track's data from the gql query GET_TRACK
+ * and provides it to the TrackDetail component to display
+ */
 export const Track = ({trackId}) => {
     const {loading, error, data } = useQuery(GET_TRACK, {
         variables: {trackId}
     });
-    console.log(JSON.stringify(data));
   return (
-    <Layout>layout</Layout>
+    <Layout>
+        <TrackDetail track={data?.track} />
+    </Layout>
   )
 };
