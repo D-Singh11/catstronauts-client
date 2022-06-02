88c61fbca132a94aee39aa30351bb9019ff8f4ac
setname <setemail>
2022-06-02 13:07:25 +0100

    Create query to get data and console log it

--- catstronauts-client/src/pages/module.js
+++ catstronauts-client/src/pages/module.js
@@ -1,8 +1,36 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react'
 import { Layout } from '../components';
 
-function Module(props) {
+const GET_MODULE_TRACK = gql`
+    query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
+        module(id: $moduleId) {
+            id
+            title
+            content
+            videoUrl
+        }
+        track(id: $trackId) {
+            id
+            title
+            modules {
+            id
+            title
+            length
+            }
+        }
+    }
+`;
 
+function Module(props) {
+    const {trackId, moduleId}= props;
+
+    console.log(props);
+    const {loading, error, data } = useQuery(GET_MODULE_TRACK, {
+        variables: {moduleId, trackId}
+    })
+
+    console.log(data);
   return (
       <Layout fullWidth>
       </Layout>
