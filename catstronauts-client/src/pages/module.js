4e48674a65880132faf078c8a25174fcdbc491a5
setname <setemail>
2022-06-02 13:04:56 +0100

    Create Module component

---
+++ catstronauts-client/src/pages/module.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../components';
+
+function Module(props) {
+
+  return (
+      <Layout fullWidth>
+      </Layout>
+  )
+}
+
+export default Module;
